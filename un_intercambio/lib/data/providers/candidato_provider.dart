7773b32f4a5a2a7771041139f046fa2f079276b7
Hinara Pastora Sánchez Mata <69545933+HinaraSM@users.noreply.github.com>
2025-03-02 13:15:11 -0500

    adding candidatos

---
+++ un_intercambio/lib/data/providers/candidato_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:un_intercambio/data/repositories/candidato._repository.dart';
+import '../models/candidato.dart';
+
+final candidatosProvider = FutureProvider.family<List<Candidato>, int>((ref, convocatoriaId) async {
+  final repository = CandidatoRepository();
+  return repository.fetchCandidatosPorConvocatoria(convocatoriaId);
+});
