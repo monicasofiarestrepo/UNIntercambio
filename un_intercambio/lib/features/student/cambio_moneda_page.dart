bde0cdf8ce302c5f4a513beb38beb34154cad14c
Camilog2004 <juagutierrezma@unal.edu.co>
2025-03-02 17:14:26 -0500

    mejoras a pantalla de divisas

--- un_intercambio/lib/features/student/cambio_moneda_page.dart
+++ un_intercambio/lib/features/student/cambio_moneda_page.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:un_intercambio/core/theme.dart';
 import 'package:un_intercambio/data/providers/exchange_rate_provider.dart';
 import 'package:country_flags/country_flags.dart';
 
 class CurrencyConverterScreen extends ConsumerStatefulWidget {
   const CurrencyConverterScreen({super.key});
   @override
-  _CurrencyConverterScreenState createState() => _CurrencyConverterScreenState();
+  _CurrencyConverterScreenState createState() =>
+      _CurrencyConverterScreenState();
 }
 
-class _CurrencyConverterScreenState extends ConsumerState<CurrencyConverterScreen> {
+class _CurrencyConverterScreenState
+    extends ConsumerState<CurrencyConverterScreen> {
   final TextEditingController _amountController = TextEditingController();
   String selectedCurrency = "USD";
+  String confirmedCurrency = "USD";
   double convertedAmount = 0.0;
+  double lastUsedRate = 1.0; // Guarda la última tasa de conversión utilizada
 
   final List<Map<String, String>> currencies = [
     {"code": "USD", "flag": "US"},
@@ -22,10 +27,11 @@ class _CurrencyConverterScreenState extends ConsumerState<CurrencyConverterScree
     {"code": "BRL", "flag": "BR"},
   ];
 
-  void convertCurrency(double rate) {
+  void convertCurrency() {
     final double amount = double.tryParse(_amountController.text) ?? 0;
     setState(() {
-      convertedAmount = amount * rate;
+      convertedAmount = amount * lastUsedRate;
+      confirmedCurrency = selectedCurrency;
     });
   }
 
@@ -35,78 +41,115 @@ class _CurrencyConverterScreenState extends ConsumerState<CurrencyConverterScree
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Convertidor de Moneda"),
-        backgroundColor: Colors.deepPurple,
+        title: const Text(
+          "Convertidor de divisas",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
       ),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Card(
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  TextField(
-                    controller: _amountController,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      labelText: "Monto en COP",
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  DropdownButton<String>(
-                    value: selectedCurrency,
-                    onChanged: (newValue) {
-                      setState(() {
-                        selectedCurrency = newValue!;
-                      });
-                    },
-                    items: currencies.map((currency) {
-                      return DropdownMenuItem<String>(
-                        value: currency["code"],
-                        child: Row(
-                          children: [
-                            CountryFlag.fromCountryCode(currency["flag"]!, height: 20, width: 30),
-                            const SizedBox(width: 10),
-                            Text(currency["code"]!),
-                          ],
-                        ),
-                      );
-                    }).toList(),
-                  ),
-                  const SizedBox(height: 16),
-                  exchangeRates.when(
-                    data: (data) {
-                      final num rate = data.conversionRates[selectedCurrency] ?? 1;
-                      return Column(
-                        children: [
-                          ElevatedButton(
-                            onPressed: () => convertCurrency(rate.toDouble()),
-                            child: const Text("Convertir"),
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.deepPurple,
-                              foregroundColor: Colors.white,
-                              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 24),
-                            ),
-                          ),
-                          const SizedBox(height: 16),
-                          Text(
-                            "Total: ${convertedAmount.toStringAsFixed(2)} $selectedCurrency",
-                            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      );
-                    },
-                    loading: () => const CircularProgressIndicator(),
-                    error: (err, stack) => const Text("Error al cargar tasas"),
-                  ),
-                ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                "Aquí podrás ver el equivalente de dinero colombiano en las principales monedas del mundo.",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
-            ),
+              const SizedBox(height: 20),
+              Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12)),
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextField(
+                        controller: _amountController,
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          labelText: "Monto en COP",
+                          prefixText: "\$ ",
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      // Texto que indica a qué moneda se está convirtiendo
+                      Text(
+                        "a : ",
+                        style: const TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 16),
+                      DropdownButton<String>(
+                        value: selectedCurrency,
+                        onChanged: (newValue) {
+                          setState(() {
+                            selectedCurrency = newValue!;
+                          });
+                        },
+                        items: currencies.map((currency) {
+                          return DropdownMenuItem<String>(
+                            value: currency["code"],
+                            child: Row(
+                              children: [
+                                CountryFlag.fromCountryCode(
+                                    currency["flag"]!,
+                                    height: 20,
+                                    width: 30),
+                                const SizedBox(width: 10),
+                                Text(currency["code"]!),
+                              ],
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                      const SizedBox(height: 16),
+                      exchangeRates.when(
+                        data: (data) {
+                          lastUsedRate =
+                              (data.conversionRates[selectedCurrency] ?? 1)
+                                  .toDouble();
+
+                          return Column(
+                            children: [
+                              ElevatedButton(
+                                onPressed: convertCurrency,
+                                child: const Text("Convertir"),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: SystemColors.primaryPrink,
+                                  foregroundColor: Colors.white,
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 12, horizontal: 24),
+                                ),
+                              ),
+                              const SizedBox(height: 16),
+                              Text(
+                                "Total: ${convertedAmount.toStringAsFixed(2)} $confirmedCurrency",
+                                style: const TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          );
+                        },
+                        loading: () => const CircularProgressIndicator(),
+                        error: (err, stack) =>
+                            const Text("Error al cargar tasas"),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
